1e9f486f524a8f3392ba19a9cb89390cdafe7a3a
Brian Walden <brian@slpalaska.com>
2026-02-27 12:44:53 -0900

    Fix div nesting build error

--- app/investigation-workbench/[id]/page.js
+++ app/investigation-workbench/[id]/page.js
@@ -1108,6 +1108,6 @@ ${lessonsLearned.length?`<div class="section"><div class="section-title">Lessons
     </div>
 
     <div style={st.footer}><span style={{fontWeight:'500'}}>AnthroSafe{'\u2122'} Field Driven Safety</span> | {'\u00A9'} 2026 SLP Alaska, LLC</div>
-    </div></div>
+    </div>
   );
 }
